1219bce56a813d182f309073a91bbeb8bf6ea9e0
Xiwang <thegostdev@outlook.com>
2024-03-18 22:53:23 +0100

    random combat starter

--- ConsoleApp1/Class/Heroes.cs
+++ ConsoleApp1/Class/Heroes.cs
@@ -17,7 +17,9 @@ namespace Main.Class
         private int m_iMana;
         private float m_fDamage;
         private float m_fResistance;
-        private float m_fSpeed;
+        private int m_iSpeed;
+        private int m_iIVSpeed = 0;
+        private int m_iFinalSpeed;
         private float m_fPrecision;
         private float m_fExp;
         private int m_iLevel;
@@ -32,7 +34,9 @@ namespace Main.Class
         public int Mana { get => m_iMana; set => m_iMana = value; }
         public float Damage { get => m_fDamage; set => m_fDamage = value; }
         public float Resistance { get => m_fResistance; set => m_fResistance = value; }
-        public float Speed { get => m_fSpeed; set => m_fSpeed = value; }
+        public int Speed { get => m_iSpeed; set => m_iSpeed = value; }
+        public int IVSpeed { get => m_iIVSpeed; set => m_iIVSpeed = value; }
+        public int FinalSpeed { get => m_iFinalSpeed; set => m_iFinalSpeed = value; }
         public float Precision { get => m_fPrecision; set => m_fPrecision = value; }
         public float Exp { get => m_fExp; set => m_fExp = value; }
         public int Level { get => m_iLevel; set => m_iLevel = value; }
@@ -49,12 +53,13 @@ namespace Main.Class
         public event Action onChangedPrecision;
         public event Action onChangedExp;
         public event Action onChangedLevel;
+        public event Action onChangedIVSpeed;
 
         #endregion
 
         #region Methods
 
-        public Heroes(string sName, int iHp, int iMana, float fDamage, float fResistance, float fSpeed, float fPrecision, Types cType)
+        public Heroes(string sName, int iHp, int iMana, float fDamage, float fResistance, int iSpeed, float fPrecision, Types cType)
         {
 
             m_sName = sName;
@@ -62,7 +67,8 @@ namespace Main.Class
             m_iMana = iMana;
             m_fDamage = fDamage;
             m_fResistance = fResistance;
-            m_fSpeed = fSpeed;
+            m_iSpeed = iSpeed;
+            m_iFinalSpeed = iSpeed + m_iIVSpeed;
             m_fPrecision = fPrecision;
             m_fExp = 0;
             m_iLevel = 0;
@@ -85,9 +91,9 @@ namespace Main.Class
             m_fResistance += fResistance;
             onChangedResistance?.Invoke();
         }
-        public void UpdateSpeed(float fSpeed)
+        public void UpdateSpeed(int iSpeed)
         {
-            m_fSpeed += fSpeed;
+            m_iSpeed += iSpeed;
             onChangedSpeed?.Invoke();
 
         }
@@ -105,6 +111,36 @@ namespace Main.Class
         {
             m_iLevel += iLevel;
             onChangedLevel?.Invoke();
+        }
+        public void UpdateIVSpeed(int iIVSpeed)
+        {
+            m_iIVSpeed += iIVSpeed;
+            onChangedIVSpeed?.Invoke();
+        }
+
+        public void GenerateIVSpeed()
+        {
+            Random random = new Random();
+            int randomIVSpeed = random.Next(1, 101);
+            m_iIVSpeed = randomIVSpeed;
+            m_iFinalSpeed = m_iSpeed + m_iIVSpeed;
+        }
+
+        public static void CalculateWhoIsStarting(Heroes h1, Heroes h2)
+        {
+            int iSpeedDiff = Math.Abs(h1.FinalSpeed - h2.FinalSpeed);
+
+            double dAttackProbabilityH1 = (double)iSpeedDiff / 100;
+            double dAttackProbabilityH2 = 1 - dAttackProbabilityH1;
+
+            Random random = new Random();
+            double dAttackerProbability = random.NextDouble();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("dAttackerProbability => " + dAttackerProbability);
+            Console.WriteLine("h1 => " + dAttackProbabilityH1);
+            Console.WriteLine("h2 => " + dAttackProbabilityH2);
+            Console.WriteLine("Combat Starter: " + (dAttackerProbability < dAttackProbabilityH1 ? h1.Name : h2.Name));
         }
 
         #endregion
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,6 @@ using Main.Enum;
 using System;
 using System.Security.Cryptography.X509Certificates;
 
-
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program 
@@ -13,16 +12,25 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             map test = new map();
 
-            Heroes hTest = new Heroes("Fire-pokemon", 100, 500, 30.0f, 15.0f, 2.0f, 1.0f, Types.Fire);
+            Heroes hTest = new Heroes("Poke0", 100, 500, 30.0f, 15.0f, 50, 1.0f, Types.Fire);
+            Heroes hTest2 = new Heroes("Poke1", 500, 50, 50.0f, 10.0f, 50, 2.0f, Types.Water);
 
-            Console.WriteLine("Nom du héro: " + hTest.Name);
-            Console.WriteLine("Hp de " + hTest.Name + ": " + hTest.HP);
-            Console.WriteLine("Mana de " + hTest.Name + ": " + hTest.Mana);
-            Console.WriteLine("Damage de " + hTest.Name + ": " + hTest.Damage);
-            Console.WriteLine("Resistance de " + hTest.Name + ": " + hTest.Resistance);
-            Console.WriteLine("Speed de " + hTest.Name + ": " + hTest.Speed);
-            Console.WriteLine("Precision de " + hTest.Name + ": " + hTest.Precision);
-            Console.WriteLine("Type de " + hTest.Name + ": " + hTest.GetType);
+            //Console.WriteLine("Nom du héro: " + hTest.Name);
+            //Console.WriteLine("Hp de " + hTest.Name + ": " + hTest.HP);
+            //Console.WriteLine("Mana de " + hTest.Name + ": " + hTest.Mana);
+            //Console.WriteLine("Damage de " + hTest.Name + ": " + hTest.Damage);
+            //Console.WriteLine("Resistance de " + hTest.Name + ": " + hTest.Resistance);
+            //Console.WriteLine("Speed de " + hTest.Name + ": " + hTest.Speed);
+            //Console.WriteLine("Precision de " + hTest.Name + ": " + hTest.Precision);
+            //Console.WriteLine("Type de " + hTest.Name + ": " + hTest.GetType);
+
+            hTest.GenerateIVSpeed();
+            hTest2.GenerateIVSpeed();
+
+            //Console.WriteLine("IVSpeed Poke0: " + hTest.IVSpeed);
+            //Console.WriteLine("IVSpeed Poke1: " + hTest2.IVSpeed);
+
+            Heroes.CalculateWhoIsStarting(hTest, hTest2);
 
             test.LoadMap();
 
