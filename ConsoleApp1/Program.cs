e0e37811d069093ec51fa1870ed2945830679d92
loanalix <lalix@gaming.tech>
2024-03-19 17:58:06 +0100

    l'inventaire marche plus tôt bien

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,10 +10,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
 
-            map test = new map();
+            // map test = new map();
 
-            Heroes hTest = new Heroes("Poke0", 100, 500, 30.0f, 15.0f, 50, 1.0f, Types.Fire);
-            Heroes hTest2 = new Heroes("Poke1", 500, 50, 50.0f, 10.0f, 50, 2.0f, Types.Water);
+            //Heroes hTest = new Heroes("Poke0", 100, 500, 30.0f, 15.0f, 50, 1.0f, Types.Fire);
+            //Heroes hTest2 = new Heroes("Poke1", 500, 50, 50.0f, 10.0f, 50, 2.0f, Types.Water);
 
             //Console.WriteLine("Nom du héro: " + hTest.Name);
             //Console.WriteLine("Hp de " + hTest.Name + ": " + hTest.HP);
@@ -24,15 +24,24 @@ namespace MyApp // Note: actual namespace depends on the project name.
             //Console.WriteLine("Precision de " + hTest.Name + ": " + hTest.Precision);
             //Console.WriteLine("Type de " + hTest.Name + ": " + hTest.GetType);
 
-            hTest.GenerateIVSpeed();
-            hTest2.GenerateIVSpeed();
+            //hTest.GenerateIVSpeed();
+            //hTest2.GenerateIVSpeed();
 
             //Console.WriteLine("IVSpeed Poke0: " + hTest.IVSpeed);
             //Console.WriteLine("IVSpeed Poke1: " + hTest2.IVSpeed);
 
-            Heroes.CalculateWhoIsStarting(hTest, hTest2);
+            //Heroes.CalculateWhoIsStarting(hTest, hTest2);
 
-            test.LoadMap();
+            //test.LoadMap();
+
+
+            Potion healthPotion = new Potion("Potion de santé", 50);
+            Inventory inventory = new Inventory();
+            inventory.AddItem(healthPotion);
+            inventory.UseItem(0);
+
+
+            Console.ReadLine();
 
 
         }
