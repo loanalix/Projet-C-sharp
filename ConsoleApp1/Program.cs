7c5dcaeb725d6be2352a703e7329bc7891084b4b
jbois <jbois@gaming.tech>
2024-03-19 15:58:43 +0100

    la map est good

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,39 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
 
             map test = new map();
-
             test.LoadMap();
+            test.DrawMap();
+
+            bool isRunning = true;
+
+            ConsoleKeyInfo input;
+            
+
+            while (isRunning)
+            {
+
+                input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.UpArrow)
+                {
+                    test.Move("up");
+                }
+                else if (input.Key == ConsoleKey.DownArrow)
+                {
+                    test.Move("down");
+                }
+                else if (input.Key == ConsoleKey.RightArrow)
+                {
+                    test.Move("right");
+
+                }
+                else if (input.Key == ConsoleKey.LeftArrow)
+                {
+                    test.Move("left");
+                }
+                test.DrawMap();
+
+            }
+
 
 
         }
--- ConsoleApp1/map.cs
+++ ConsoleApp1/map.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,81 +10,120 @@ namespace Main
 {
     public class map
     {
-        static string loadMap = "";
 
-        public string Map()
-        {
-            
-            loadMap += "------------------------------------";
-            return loadMap;
-        }
+        private List<char> m_lMap = new List<char>();
 
-        public string LoadMap()
+        private List<int> m_lSpawnable = new List<int>();
+        public List<char> GetMap { get => m_lMap;}
+
+        private int iSizeLine;
+        public void LoadMap()
         {
             StreamReader reader = File.OpenText("../../../map.txt");
+            string sizeLine = reader.ReadLine();
+            iSizeLine = sizeLine.Length;
             string line;
+           
             while ((line = reader.ReadLine()) != null)
             {
-                char[] characters = line.ToCharArray();
-
-                for(int i = 0; i < characters.Length - 1; i++) {
-                    char currentChar = characters[i];
-                    char nextChar = characters[i + 1];
-                    
-                    
-                    if(currentChar == ' ' && nextChar == 'v')
-                    {
-                        Console.BackgroundColor = ConsoleColor.DarkGreen;
-                        Console.Write(currentChar);
-                        i++;
-                    }
-
-                    else if (currentChar == ' ' && nextChar == 'g')
-                    {
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.Write(currentChar);
-                        i++;
-                    }
-                    else if (currentChar == ' ' && nextChar == 'p')
-                    {
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.Write(currentChar);
-                        i++;
-                    }
-                    else if (currentChar == ' ' && nextChar == 'h')
-                    {
-                        Console.BackgroundColor = ConsoleColor.White;
-                        Console.Write(currentChar);
-                        i++;
-                    }
-                    else if (currentChar == ' ' && nextChar == 'w')
-                    {
-                        Console.BackgroundColor = ConsoleColor.DarkYellow;
-                        Console.Write(currentChar);
-                        i++;
-                    }
-                    else if (currentChar == ' ' && nextChar == 'b')
-                    {
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                        Console.Write(currentChar);
-                        i++;
-                    }
-                   
-                    else
-                    {
-                        Console.ResetColor();
-                        Console.Write(currentChar);
-                    }
+                char[] cChar= line.ToCharArray();
+                for (int i = 0; i < cChar.Length; i++)
+                {
+                    m_lMap.Add(cChar[i]);
                 }
-
-                Console.ResetColor();
-
-                Console.Write(characters[characters.Length - 1]);
-
-                Console.WriteLine();
             }
-            return loadMap;
         }
+        public void DrawMap()
+        {
+            for(int i = 0; i< m_lMap.Count; i++)
+            {
+                switch (m_lMap[i]) {
+                    case 'v':
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        Console.Write(' ');
+                        break;
+                    case 'g':
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.Write(' ');
+                        break;
+                    case 'b':
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.Write(' ');
+                        break;
+                    case 'w':
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.Write(' ');
+                        break;
+                    case 'h':
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.Write(' ');
+                        break;
+                    case 'p':
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.Write(' ');
+                        break;
+                    case '*':
+                        //Console.ResetColor();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('*');
+                        break;
+                    case ' ':
+                        Console.ResetColor();
+                        Console.Write(' ');
+                        break;
+                    case '/':
+                        Console.ResetColor();
+                        Console.Write('\n');
+                        break;
+  
+                    default:
+                        Console.Write(' ');
+                        break;
 
+
+                }
+            }
+        }
+         
+        private void UpdateMap(int iIndice, string sMove)
+        {
+            
+            int move = m_lMap.FindIndex(chara => chara == '*');
+            int iMovement;
+            if (sMove == "down" || sMove == "right")
+            {
+                iMovement = move + iIndice;
+            }
+            else
+            {
+                iMovement = move - iIndice;
+            }
+
+            m_lMap.RemoveAt(move);
+            m_lMap.Insert(move, 'p');
+            m_lMap.RemoveAt(iMovement);
+            m_lMap.Insert(iMovement, '*');
+
+        }
+        public void Move(string sCharPressed)
+        {
+
+            Console.SetCursorPosition(0, 0);
+            switch (sCharPressed)
+            {
+                case "up":
+                    UpdateMap(iSizeLine, "up");
+                    break;                    
+                case "down":
+                    UpdateMap(iSizeLine, "down");
+                    break;
+                case "left":
+                    UpdateMap(1, "left");
+                    break;
+                case "right":
+                    UpdateMap(1, "right");
+                    break;
+            }
+        }
     }
 }
