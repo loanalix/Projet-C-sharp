d776e712c47e8bbf6e9334510ad88250271b5085
loanalix <lalix@gaming.tech>
2024-03-26 19:08:22 +0100

    euuuu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,15 +21,33 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static FightManager m_oFightManager;
         public static void Main(string[] args)
         {
+            m_oGameManager = new GameManager();
+            fightManager = new FightManager();
+            m_oDrawMob = new DrawMob();
+            Heroes dialga = new Heroes("Dialga", 1200, 200, 75f, 70f, 70, Types.Grass); // Tout ça à yonk si vous voulez
+            Heroes zekrom = new Heroes("Zekrom", 1300, 210, 80f, 76f, 45, Types.Fire);
+            Heroes darkrai = new Heroes("Darkrai", 850, 90, 100f, 45f, 45, Types.Dragon);
+            Ennemy pikachu = new Ennemy("Pikachu", 150, 20, 2f, 0f, 100, Types.Flying);
+            Ennemy bulbizarre = new Ennemy("Bulbizarre", 150, 20, 2f, 0f, 100, Types.Grass);
+            Ennemy salameche = new Ennemy("Salameche", 150, 20, 2f, 0f, 100, Types.Fire);
+            Ennemy rayquaza = new Ennemy("Rayquaza", 150, 20, 2f, 0f, 100, Types.Dragon);
+            Ennemy artikodin = new Ennemy("Artikodin", 150, 20, 2f, 0f, 100, Types.Flying);
+            Ennemy giratina = new Ennemy("Giratina", 150, 20, 2f, 0f, 100, Types.Flying);
+            Ennemy mewtwo = new Ennemy("Mewtwo", 150, 20, 2f, 0f, 100, Types.Grass);
+            Ennemy lunala = new Ennemy("Lunala", 150, 20, 2f, 0f, 100, Types.Flying);
+            Ennemy groudon = new Ennemy("Groudon", 150, 20, 2f, 0f, 100, Types.Fire);
+            fightManager.LoadFightMenu();
+            fightManager.LoadFight();
+            //while (true)
+            //{
+            //    Console.CursorVisible = false;
+            //    fightManager.FightSteps();
+            //}
 
-            m_oMap = new Map();
-            m_oPlayer = new Player();
-            m_oDraw = new Draw();  
-            
-            m_oInputManager = new InputManager();
-            
-            m_oDraw.LoadMap("../../../txt/map.txt");
-            m_oDraw.DrawMap(m_oPlayer);
+            string Pokemon = "../../../txt/darceaufeu.txt";
+            string Pikachu = "../../../txt/pikachu.txt"; 
+            m_oDrawMob.LoadMob(Pikachu);
+            m_oDrawMob.LoadMob(Pokemon);
 
             Console.CursorVisible = false;
             bool isRunning = true;
@@ -41,24 +59,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Mob hTest3 = new Mob("Poke3", 500, 50, 50.0f, 10.0f, 50, Types.Dragon);
 
             m_oFightManager.StartFight();
-
-
-            Potion healthPotion = new Potion("Potion de santé", 50);
-            Inventory inventory = new Inventory();
-            inventory.AddItem(healthPotion);
-            inventory.UseItem(0);
-
-            while (isRunning)
-            {
-                Console.SetCursorPosition(0, 0);
-                m_oInputManager.GetInput(m_oPlayer, m_oDraw);
-                m_oDraw.DrawMap(m_oPlayer);
-            }
-
-
-
-
-
 
         }
     }
